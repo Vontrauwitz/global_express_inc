d1de409ade8026ef8ee5e11b13c55e25b7314a6c
Hans Trauwitz <hans.trauwitz@gmail.com>
2024-06-26 20:45:31 -0400

    minor weaks to the feature jobs

--- components/JobCarousel.js
+++ components/JobCarousel.js
@@ -1,4 +1,5 @@
 // components/JobCarousel.js
+
 import React from 'react';
 import Slider from 'react-slick';
 import Card from './Card';
@@ -24,18 +25,49 @@ const JobCarousel = () => {
   ];
 
   const settings = {
-    dots: false,
+    dots: true,
     infinite: true,
     speed: 500,
     slidesToShow: 3,
     slidesToScroll: 3,
     autoplay: true,
-    autoplaySpeed: 4000,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+          centerMode: true,
+          centerPadding: '0' // Aumentamos el margen lateral
+        }
+      }
+    ]
   };
 
   return (
-    <div className="p-8">
-     <h2 className="text-4xl font-extrabold text-center mb-8 text-color-white">Featured Jobs</h2>
+    <div className="p-8 z-10">
+      <h2 className="text-4xl font-extrabold text-center mb-8 text-dark-blue">Featured Jobs</h2>
       <Slider {...settings}>
         {jobPositions.map((job, index) => (
           <div key={index}>
